79aab6de14fdb880774e0d3298b43328f0d39a6b
cadu <cadu@2km.com.br>
2022-07-27 10:04:57 +0100

    feature: add home, some components and tailwind

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+<footer className="p-6 max-w-4xl mx-auto">
+  <p>Here comes the footer</p>
+</footer>
+  )
+}
+
+export default Footer
---
+++ components/Menu.jsx
@@ -0,0 +1,17 @@
+import logo from '../public/logo.svg'
+import Image from 'next/image'
+import Link from 'next/link'
+
+function Menu() {
+  return (
+    <nav className='flex items-center gap-2 mb-6'>
+      <Image src={logo} alt="Logo" className='w-20' />
+      <a href="#contact">About Us</a>
+      <a href="#contact">Events</a>
+      <a href="#contact">Whitepaper</a>
+      <a href="#contact">Contact</a>
+    </nav>
+  )
+}
+
+export default Menu
