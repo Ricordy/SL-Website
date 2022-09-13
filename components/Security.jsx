5ec0c838d14e090bd11aa27cf0b34c7816a3f610
cadu <cadu@2km.com.br>
2022-09-13 16:00:24 +0100

    style: change text and bg color on security page

--- components/Security.jsx
+++ components/Security.jsx
@@ -1,19 +1,21 @@
-import { useTranslation } from 'next-i18next'
+import { useTranslation } from "next-i18next";
 
 function Security() {
-  const { t } = useTranslation('security');
+  const { t } = useTranslation("security");
   return (
-    <section id="security" className='bg-slate-300 text-slate-800'>
+    <section id="security" className="bg-slate-700 text-slate-100">
       <div className="content">
-      <h2 className="text-3xl mb-12 font-serif">{t("security.section-title")}</h2>
-      <ul>
-        <li>{t("security.audit")}</li>
-        <li>{t("security.gdpr")}</li>
-        <li>{t("security.funds")}</li>
-      </ul>
+        <h2 className="text-3xl mb-12 font-serif">
+          {t("security.section-title")}
+        </h2>
+        <ul>
+          <li>{t("security.audit")}</li>
+          <li>{t("security.gdpr")}</li>
+          <li>{t("security.funds")}</li>
+        </ul>
       </div>
     </section>
-  )
+  );
 }
 
-export default Security
+export default Security;
