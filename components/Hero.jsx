27185d5355e33bf5e8c15077890804aa59d5b451
cadu <cadu@2km.com.br>
2022-09-14 14:31:01 +0100

    feature: remove slogan

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,6 @@ function Hero(props) {
   return (
     <section className=" bg-gradient-to-tr from-slate-700 to-slate-800 flex flex-col py-12 min-h-screen items-center justify-center">
       <div className="content flex">
-        <p className="font-serif text-3xl md:text-5xl">{t("slogan")}</p>
         <div className="car pt-4 w-1/2 h-1/2">
           <Image src={car} alt="Classic Car" className="" />
         </div>
