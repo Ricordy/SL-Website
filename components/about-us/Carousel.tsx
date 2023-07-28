001ecfc51de812703e73905c2b7e55bcc35af1e6
Ricordy <youberrody@gmail.com>
2023-07-28 11:19:43 +0100

    Replace images, names and positions CHIEFS about u

--- components/about-us/Carousel.tsx
+++ components/about-us/Carousel.tsx
@@ -8,51 +8,77 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import { NOMEM } from "dns";
 // import "swiper/css/navigation";
 
 interface CarouselItemProps {
   title: string;
   image: string;
-  status: string;
-  completion: string;
+  status?: string;
+  completion?: string;
+  name?: string;
+  lastName?: string;
+  description?: string;
 }
+
+const chiefsInfo = [
+  {
+    title: "Chief of Finantial Office",
+    name: "Gonçalo",
+    lastName: "Severino",
+    image: "/chiefs/Gonçalo.svg",
+    description: `Gonçalo Severino, formação em gestão. Desde cedo, senti que o ensino escolar não era suficiente para atingir a alta performance no mundo supersónico em que vivemos. Isso motivou-me pessoalmente a ingressar no mundo económico e financeiro para realmente compreender e aprender como funciona uma das vertentes mais importantes da sociedade de hoje em dia. Isso acabou por me levar ao mercado de ações aos 18 anos. Cedo ganhei interesse pelo mercado de Criptomoedas e, posteriormente, de NFTs. Decidi que não queria ser mais um peixe no mar e a minha grande preocupação no primeiro ano não foi ganhar dinheiro, mas sim entender realmente como funciona. Quando ainda estava no meu segundo ano de faculdade, comecei a prestar serviços de consultoria a empresários individuais e a algumas empresas, nomeadamente agências de publicidade e marketing. No início de 2022, comecei a trabalhar com o meu atual sócio onde formamos uma empresa (Web 360 Labs) de prestação de serviços 360º em tudo o que toca à Web 3.0 (blockchain, criptomoedas, NFTs), desde o aconselhamento legal e financeiro, ao desenvolvimento "hands-on" informático e tudo o que toca na criação de marca, marketing e definição do modelo de negócio. O nosso objetivo é transformar ideias em produtos prontos para o consumidor final.`,
+  },
+  {
+    title: "Chief Executive Officer",
+    name: "João",
+    lastName: "Fernandes",
+    image: "/chiefs/gonçalo2.svg",
+    description: `I'm a person with a variety of interests and experiences, but one thing that has always captivated me is classic cars. As an entrepreneur and investor, I've had the opportunity to establish successful companies in several industries, including architecture, restaurants, and real estate investments. I was also part of the team that founded a company in stem cells in the UK that was listed on the stock market. My passion for innovation has also led me to establish a web3 consulting company, where I can explore new and exciting ideas in the tech space.
+
+My extensive travels around the world and establishment of companies on four different continents have given me a global perspective and the ability to adapt to diverse cultures and markets. Additionally, as the honorary consul of Albania, I'm dedicated to public service and community engagement.
+
+However, it's my love for investing in and restoring classic cars that lead me to create Something Legendary.  With my unique combination of expertise in classic cars and web3 technology, I believe I can bring fresh ideas and new approaches to the classic car industry. Ultimately, my goal is to make a positive impact on the world and leave a lasting legacy through my work.`,
+  },
+  {
+    title: "BOSS of repairs",
+    name: "João",
+    lastName: "Dias",
+    image: "/chiefs/gonçalo3.jpg",
+    description: `João Dias, formação de engenharia, 32 anos, nos últimos 15 ligado e no meio dos carros desportivos e clássicos. Com formação feita e experiência ganha entre oficinas de restauro, colecionadores, corridas e concursos, foi chefe de oficina com apenas 22 anos liderando uma equipa de 17 "mestres" do restauro pelo conhecimento, gosto e capacidade de fazer acontecer. Em 2016 cria a True Legend e destaca-se pela organização, critério e qualidade. Vários prémios de restauro ganhos em Portugal e Alemanha, presença nos mais importantes eventos internacionais, projectos de restauro muito especiais e visão das possibilidades de investimento que o mercado tem para oferecer.`,
+  },
+];
 
 const CarouselItem = ({
   title,
   image,
   status,
   completion,
+  name,
+  lastName,
+  description,
 }: CarouselItemProps) => {
   return (
     <div className="flex w-[850px] gap-14 rounded-md">
       <div
-        className={cn("flex w-1/2 rounded-md bg-cover relative gap-3", image)}
+        className={cn("flex w-1/2 rounded-md bg-cover relative gap-3 ", image)}
       >
         <Image
           src={image}
-          className="rounded-lg"
+          className="rounded-lg "
           width={440}
           height={394}
           alt="Gonçalo Severino"
         />
       </div>
       <div className="flex flex-col justify-start items-start w-1/2 pt-[52px]">
-        <span className="text-tabInactive uppercase">
-          Chief Financial Officer
-        </span>
+        <span className="text-tabInactive uppercase">{title}</span>
         <h3 className="uppercase pb-8">
-          <span className="text-primaryGreen">Gonçalo</span>
+          <span className="text-primaryGreen">{name}</span>
           <br />
-          Severino
+          {lastName}
         </h3>
-        <p className="text-left">
-          João Dias, engineering training, 32 years of which in the last 15 he
-          was involved in sports and classic cars. With training done and
-          experience gained in restoration workshops, collectors, races and
-          competitions, he was workshop manager at just 22 years old leading a
-          team of 17 &quot;masters&quot; of restoration for their knowledge,
-          taste and ability to make things happen.
-        </p>
+        <p className="text-left">{description}</p>
       </div>
     </div>
   );
@@ -62,28 +88,6 @@ interface CarouselProps {
   id: string;
   className?: string;
 }
-
-const items = [
-  {
-    title: "Maserati 3500 GT",
-    image: "/about-us/cfo.jpg",
-    status: "Renewing",
-    completion: "15",
-  },
-  {
-    title: "Chevrolet 200",
-    image: "/about-us/cfo.jpg",
-    status: "Renewing",
-    completion: "45",
-  },
-  ,
-  {
-    title: "Chevrolet 300",
-    image: "/about-us/cfo.jpg",
-    status: "Renewing",
-    completion: "85",
-  },
-];
 
 const Carousel: FC<CarouselProps> = ({ id, className }) => {
   return (
@@ -115,13 +119,14 @@ const Carousel: FC<CarouselProps> = ({ id, className }) => {
             initialSlide={0}
             // loop={true}
           >
-            {items.map((item, index) => (
+            {chiefsInfo.map((item, index) => (
               <SwiperSlide key={index}>
                 <CarouselItem
                   title={item.title}
                   image={item.image}
-                  status={item.status}
-                  completion={item.completion}
+                  name={item.name}
+                  description={item.description}
+                  lastName={item.lastName}
                 />
               </SwiperSlide>
             ))}
