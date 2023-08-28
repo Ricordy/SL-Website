272a00c795cf53ed92221213745107ba2cf1b806
Ricordy <youberrody@gmail.com>
2023-08-28 18:23:17 +0100

    fixed faqs

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -4,15 +4,11 @@ import Accordion from "../components/faq/Accordion";
 
 const faq = () => {
   return (
-    <section className="flex w-full gap-[132px]  flex-col">
+    <section className="flex w-full gap-[132px]  flex-col pb-[132px]">
       <div className="flex max-w-screen-lg pt-[52px] w-full mx-auto gap-[52px]  items-center flex-col">
         <h3 className="font-light">FAQ&apos;S</h3>
         <Accordion />
       </div>
-      <Contact
-        title="Something in your
-legendary mind?"
-      />
     </section>
   );
 };
